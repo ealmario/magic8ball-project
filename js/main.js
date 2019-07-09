76db0f2f6bf79a12b8fda64b5ca97ba1fdbf620b
Eli Almario <ee.almario@gmail.com>
2019-07-10 01:06:20 +0800

    Fixed the Javascript file, added a footer, added a modal.

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
-var predictBtn = document.getElementById("predict-button");
-var questionInput = document.getElementById("question-input").value;
-var freezeLayer = document.getElementById("freezeLayer");
-var answerWindow = document.getElementById("answer-window");
-var answerText = document.getElementById("answer-text");
-var okayBtn = document.getElementById("okay-btn");
+var question = document.getElementById('question-input').value;
+var answerText = document.getElementById('answer-text');
+var predictBtn = document.getElementById('predict-button');
+var okayBtn = document.getElementById('okay-btn');
 
-var magic8Ball={};
-    magic8Ball.listOfAnswers = ["Definitely!", "Very Likely!", "Hell, yea!", "Nopey Nope!", "Don\'t Count On It!", "Odds Not In Your Favor!", "Future Hazy", "Too Lazy To Predict", "Ask Again Later"];
-    magic8Ball.askQuestion = function(questionInput) {
-        // Get Random Number
-        var random = Math.random();
-
-        // Random Number * Array Length
-        var randomNumberArray = random * this.listOfAnswers.length;
-
-        // Round Off
+var magic8Ball = {};
+	magic8Ball.listOfAnswers = ["Definitely!", "Very Likely!", "Hell, yea!", "Nopey Nope!", "Don\'t Count On It!", "Odds Not In Your Favor!", "Future Hazy", "Too Lazy To Predict", "Ask Again Later", "Erm, Maybe", "Not sure", "Don\'t Know"];
+	magic8Ball.askQuestion = function(question) {
+        //randomNumber
+        var randomNumber = Math.random();
+        //Random Number * Array length
+        var randomNumberArray = randomNumber*this.listOfAnswers.length;
+        //Round Off
         var randomIndex = Math.floor(randomNumberArray);
-
-        //Get Random Answer
+        // Random Answer
         var answerMe = this.listOfAnswers[randomIndex];
 
-        answerText = answerMe;
-
+        answerText.innerText = answerMe;
     }
-
-predictBtn.addEventListener("click", magic8Ball.askQuestion(questionInput));
+    
+predictBtn.addEventListener('click', function(){
+    magic8Ball.askQuestion(question);
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+okayBtn.addEventListener('click', function(){
+    document.querySelector('.bg-modal').style.display = 'none';
+})
 
 
