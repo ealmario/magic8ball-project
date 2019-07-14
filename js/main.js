89aebea0ae1a8b319ad71eb944402d043e242dbf
Eli Almario <ee.almario@gmail.com>
2019-07-14 22:16:53 +0800

    Added placeholder attribute

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var answerText = document.getElementById('answer-text');
 var predictBtn = document.getElementById('predict-button');
 var okayBtn = document.getElementById('okay-btn');
 
+//magic 8 ball object
 var magic8Ball = {};
 	magic8Ball.listOfAnswers = ["Definitely!", "Very Likely!", "Hell, yea!", "Nopey Nope!", "Don\'t Count On It!", "Odds Not In Your Favor!", "Future Hazy", "Too Lazy To Predict", "Ask Again Later", "Erm, Maybe", "Not sure", "Don\'t Know"];
 	magic8Ball.askQuestion = function(question) {
@@ -17,11 +18,12 @@ var magic8Ball = {};
 
         answerText.innerText = answerMe;
     }
-    
+
 predictBtn.addEventListener('click', function(){
     magic8Ball.askQuestion(question);
     document.querySelector('.bg-modal').style.display = 'flex';
 });
+
 okayBtn.addEventListener('click', function(){
     document.querySelector('.bg-modal').style.display = 'none';
 })
